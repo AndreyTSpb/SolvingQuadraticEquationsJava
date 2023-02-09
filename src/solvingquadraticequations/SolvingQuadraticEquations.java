0de156bf72de7853bd5efcb2d2b889cc5d4e3477
AndreyTSpb <tynyanyi@mail.ru>
2023-02-10 00:01:16 +0300

    first commit

--- src/solvingquadraticequations/SolvingQuadraticEquations.java
+++ src/solvingquadraticequations/SolvingQuadraticEquations.java
@@ -8,13 +8,21 @@ import solvingquadraticequations.Discriminant;
  */
 public class SolvingQuadraticEquations {
 
-    private static Scanner num = new Scanner(System.in); //Для ввода с клавиатуры
+    private static Scanner scanner = new Scanner(System.in); //Для ввода с клавиатуры
+
+    private double a;
+    private double b;
+    private double c;
 
     public static void main(String[] args){
         System.out.println("Программа решения квадратных уравнений");
         System.out.println("Введите коэффициенты a, b и c для квадратного уравнения  ax2 + bx + c = 0");
-        System.out.print("Введите a: ");
-
+        System.out.print("Введите значение a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Введите значение b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Введите значение c: ");
+        double c = scanner.nextDouble();
 
     }
 
